6582f72fcafc828de48e3bd411db60cd85509ed3
vinny-paris <vinny@iastate.edu>
2017-11-17 16:38:23 -0500

    Got core liklihood/wald up and running. Also, data set up is ready to move

--- midterm.R
+++ midterm.R
@@ -3,11 +3,11 @@ library(boot)
 
 
 
-set_10_2 <- matrix(replicate(2000, rinvgauss(10, mean = 5, shape = 2)), nrow = 2000)
-set_25_2 <- rinvgauss(25, mean = 5, shape = 2)
-set_50_2 <- rinvgauss(50, mean = 5, shape = 2)
-set_100_2 <- matrix(replicate(2000, expr = rinvgauss(100, mean = 5, shape = 2)), nrow = 2000)
-set_500_2 <- matrix(replicate(2000, expr = rinvgauss(500, mean = 5, shape = 2)), nrow = 2000)
+set_10_2 <- matrix(replicate(5000, rinvgauss(10, mean = 5, shape = 2)), nrow = 5000)
+set_25_2 <- matrix(replicate(5000, expr = rinvgauss(25, mean = 5, shape = 2)), nrow = 5000)
+set_50_2 <-matrix(replicate(5000, expr = rinvgauss(50, mean = 5, shape = 2)), nrow = 5000)
+set_100_2 <- matrix(replicate(5000, expr = rinvgauss(100, mean = 5, shape = 2)), nrow = 5000)
+set_500_2 <- matrix(replicate(5000, expr = rinvgauss(500, mean = 5, shape = 2)), nrow = 5000)
 
 
 set_10_4 <- rinvgauss(10, mean = 5, shape = 4)
@@ -47,26 +47,29 @@ apply(k, MARGIN  = 1, basicglm, link = 1, xmat = rep(1,10), random = 6) ->esters
 
 
 #The log liklihood of the inverse gaussian
-loglikinvgaus <- function(data, par){
-  mu <- par[1]
-  lambda <- par[2]
+loglikinvgaus <- function( data, par){
+   mu <- par[1]
+   lambda <- par[2]
   j <- -(sum(1/2*log(lambda) - 1/2*(log(2*pi))  -3/2*log(data)) - lambda/(2*mu^2)*sum((data-mu)^2/data))
   }
 
 correct <- NULL
+
 for(i in c(1:2000)){
-  dit <- set_10_2[i,]
+  dit <- set_500_2[i,]
 #finding MLE optims
-k <- optim(c(5,2), loglikinvgaus, data = dit, hessian = T, method = 'L-BFGS-B', lower = c(.5,.5), upper = c(100,100))
+k <- optim(c(5,log(2)), loglikinvgaus, data = dit, hessian = T, method = 'L-BFGS-B', lower = c(-Inf, 0.01), upper = c(Inf, Inf))
 vcov <- solve(k$hessian)
 
+
 #confidence interval
-upper <- k$par[1] + 1.96*vcov[1,1]
-lower <- k$par[1] - 1.96*vcov[1,1]
+upper <- k$par[1] + 1.96*sqrt(vcov[1,1])
+lower <- k$par[1] - 1.96*sqrt(vcov[1,1])
 
 correct[i] <- 5 < upper & 5 > lower
+holding <- sum(correct)/2000
 }
 
 
 
-sum(correct)/2000
+
